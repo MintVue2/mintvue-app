a46d307c17a75f00aa7ad7b68b5f01bdbed1a837
Penivera <penivera655@gmail.com>
2026-05-12 09:29:03 +0100

    added logout endpoint

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -1,15 +1,24 @@
-from sqlalchemy.ext.asyncio import AsyncSession
+from typing import Annotated
+
+import redis.asyncio as redis
+from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import HTTPAuthorizationCredentials
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from app.models.user import User
-from core.security import AuthResponse, SignupRequest, LoginRequest
-from app.service.auth import signUp, login
+from app.service.auth import login, logout, signUp
+from core.redis import Container
+from core.security import (
+    AuthResponse,
+    LoginRequest,
+    SignupRequest,
+    auth_scheme,
+    get_current_user,
+)
 from core.session import get_session
 
-
-router = APIRouter(
-    prefix="/auth", 
-    tags=["auth"]
-)
+router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @router.post("/signup", response_model=AuthResponse)
@@ -22,3 +31,15 @@ async def signup_user(req: SignupRequest, db: AsyncSession = Depends(get_session
 async def login_user(req: LoginRequest, db: AsyncSession = Depends(get_session)):
     """Endpoint to login a user"""
     return await login(req, db)
+
+
+@router.post("/logout")
+@inject
+async def logout_user(
+    token: Annotated[HTTPAuthorizationCredentials, Depends(auth_scheme)],
+    current_user: User = Depends(get_current_user),
+    redis_client: redis.Redis = Depends(Provide[Container.redis_pool]),
+):
+    """Endpoint to logout a user"""
+    return await logout(token.credentials, redis_client, current_user.email)
+
